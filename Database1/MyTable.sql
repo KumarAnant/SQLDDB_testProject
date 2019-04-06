0db86f81e126baa3d96433ce676eec741a637062
KumarAnant <KumarAnant@users.noreply.github.com>
2019-04-06 12:34:04 -0700

    Add project files.

---
+++ Database1/MyTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].Employee
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [email] NCHAR(50) NULL, 
+    [first_name] NCHAR(50) NULL, 
+    [last_name] NCHAR(50) NULL
+)
---
+++ Database1/MyView.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [dbo].[MyView]
+	AS SELECT email, first_name, last_name FROM Employee
